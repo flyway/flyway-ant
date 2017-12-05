9722face92a7cc39ccfef1cafdf7cbe3ce03e42d
Tomas Tulka <tomas.tulka@gmail.com>
2017-12-05 16:13:02 +0100

    test class renamed

--- src/test/java/org/flywaydb/ant/AbstractFlywayTaskTest.java
+++ src/test/java/org/flywaydb/ant/AbstractFlywayTaskTest.java
@@ -15,26 +15,26 @@
  */
 package org.flywaydb.ant;
 
-import org.flywaydb.ant.AbstractFlywayTask;
-import org.junit.Test;
-
 import java.io.File;
+
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class AbstractFlywayTaskSmallTest {
+public class AbstractFlywayTaskTest {
+
     @Test
     public void adjustRelativeFileSystemLocationToBaseDir() {
         String root = File.listRoots()[0].getPath();
 
         File baseDir = new File("/tempo");
         assertEquals("db/migration",
-                AbstractFlywayTask.adjustRelativeFileSystemLocationToBaseDir(baseDir, "db/migration"));
+                     AbstractFlywayTask.adjustRelativeFileSystemLocationToBaseDir(baseDir, "db/migration"));
 
         assertEquals("filesystem:" + root + "test/migration",
-                AbstractFlywayTask.adjustRelativeFileSystemLocationToBaseDir(baseDir, "filesystem:" + root + "test/migration"));
+                     AbstractFlywayTask.adjustRelativeFileSystemLocationToBaseDir(baseDir, "filesystem:" + root + "test/migration"));
 
         assertEquals("filesystem:" + root + "tempo/test/migration",
-                AbstractFlywayTask.adjustRelativeFileSystemLocationToBaseDir(baseDir, "filesystem:test/migration"));
+                     AbstractFlywayTask.adjustRelativeFileSystemLocationToBaseDir(baseDir, "filesystem:test/migration"));
     }
 }
