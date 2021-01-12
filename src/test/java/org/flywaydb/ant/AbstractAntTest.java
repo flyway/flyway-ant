c66e5c99b8dd5a2365998ebd93f2dbeca3f4087d
Tomas Tulka <tomas.tulka@gmail.com>
2021-01-12 19:20:13 +0100

    copyright 2021

--- src/test/java/org/flywaydb/ant/AbstractAntTest.java
+++ src/test/java/org/flywaydb/ant/AbstractAntTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 Tomas Tulka
+ * Copyright 2017-2021 Tomas Tulka
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
